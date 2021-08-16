b3b6685951a53632ab886d189bcb7978bfa5de0e
tulasi1356 <botchatulasi1356@gmail.com>
2021-08-16 11:30:37 +0530

    creting rep

--- profile/e2e/src/app.e2e-spec.ts
+++ profile/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('editfeature app is running!');
+    expect(await page.getTitleText()).toEqual('profile app is running!');
   });
 
   afterEach(async () => {
