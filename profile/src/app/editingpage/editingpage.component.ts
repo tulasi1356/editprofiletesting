999ced7238046d7ba5e61dcf7a0fe149ce45afd8
tulasi1356 <botchatulasi1356@gmail.com>
2021-08-28 23:50:01 +0530

    before setting up json

--- profile/src/app/editingpage/editingpage.component.ts
+++ profile/src/app/editingpage/editingpage.component.ts
@@ -19,12 +19,20 @@ export class EditingpageComponent implements OnInit {
   constructor(private fb: FormBuilder) {
  
     this.form = this.fb.group({
+      caption:new FormControl('Welcome to Hawks Hospital',Validators.required),
       cauroselone: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.', Validators.required),
       cauroseltwo: new FormControl('Vestibulum quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Utmtc tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio.Antonio Moreno Web Developer', Validators.required),
       cauroselthree: new FormControl('Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia. Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt fringilla massa. Etiam hendrerit dolor eget rutrum. Michael Holz,Seo Analyst', Validators.required),
       cardtitle: new FormControl('Lorem ipsum dolor sit amet',Validators.required),
       cardtext: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.', Validators.required),
+      cardtitle2: new FormControl('Lorem ipsum dolor sit amet',Validators.required),
+      cardtext2: new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.', Validators.required),
       doctortext1:new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper mal.', Validators.required),
+      doctortext2:new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper mal.', Validators.required),
+      doctortext3:new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper mal.', Validators.required),
+      doctortext4:new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper mal.', Validators.required),
+      servicecontent:new FormControl('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper mal.', Validators.required),
+     
       // cardsarray: new FormArray([
       //   // this.initQuestion(),
       // ]),
